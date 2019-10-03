c887633c208690990f492961155f5236592b2f9e
Koen Lageveen <koen.lageveen@me.com>
2019-10-03 20:05:03 +0200

    clean up the docs

--- linter.py
+++ linter.py
@@ -1,21 +1,8 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Written by Ethan Zimmerman
-# Copyright (c) 2014 Ethan Zimmerman
-#
-# License: MIT
-#
-
-"""This module exports the RamlCop plugin class."""
-
 from SublimeLinter.lint import Linter
 
 
 class RamlCop(Linter):
 
-    """Provides an interface to raml-cop."""
     defaults = {
         "selector": "source.raml"
     }
